4986219cf382670d34f52156d5be5b9ca3963dcf
Brian Weaver <bdrums@gmail.com>
2021-10-04 13:56:05 +0000

    Use the right rubocop

--- nvim/lua/bweave/plugin_settings.lua
+++ nvim/lua/bweave/plugin_settings.lua
@@ -3,6 +3,7 @@
 -- ale
 vim.g.ale_fix_on_save = 1
 vim.g.ale_ruby_rubocop_executable = 'bundle'
+vim.g.ale_ruby_ruby_executable = '~/.rbenv/shims/ruby'
 vim.g.ale_linters = {
    ruby = {'rubocop'},
    javascript = {'eslint'},
