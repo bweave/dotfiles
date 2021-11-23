7edcb14cbfc0050b8e3b3b8efb53a9c7d1b4417d
Brian Weaver <bdrums@gmail.com>
2021-11-23 20:44:52 +0000

    Don't need this guy anymore
    Treesitter playground gives us a better function to find the syntax
    highlight group, so let's use that.

--- nvim/lua/bweave/functions.lua
+++
@@ -1,8 +0,0 @@
--- Functions
-
-local fn = vim.fn
-
-vim.fn.synGroup = function()
-  local s = fn.synID(fn.line('.'), fn.col('.'), 1)
-  print(fn.synIDattr(s, 'name') .. ' -> ' .. fn.synIDattr(fn.synIDtrans(s), 'name'))
-end
