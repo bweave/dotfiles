6c70a5aac4357d024a679ce28e49d120270679e8
Brian Weaver <bdrums@gmail.com>
2021-11-23 20:46:00 +0000

    Don't require deleted files

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 require('bweave.settings')
 require('bweave.mappings')
 require 'bweave.plugins'
-require 'bweave.plugin_settings'
 require('bweave.autocommands')
-require('bweave.functions') -- TODO: not sure how to do this, yet
 require('bweave.theme')
