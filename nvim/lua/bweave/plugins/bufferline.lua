34847c807debff0ba0f941a3d4ea72f8e2336e76
Brian Weaver <bdrums@gmail.com>
2021-12-24 12:39:14 -0500

    Bufferline tweaks

--- nvim/lua/bweave/plugins/bufferline.lua
+++ nvim/lua/bweave/plugins/bufferline.lua
@@ -3,7 +3,24 @@ paq {'akinsho/bufferline.nvim'}
 local bufferline = require 'bufferline'
 bufferline.setup {
   options = {
-    numbers = "ordinal"
+    diagnostics = "nvim_lsp",
+    --- count is an integer representing total count of errors
+    ----- level is a string "error" | "warning"
+    ----- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
+    ----- this should return a string
+    ----- Don't get too fancy as this function will be executed a lot
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end,
+    numbers = "ordinal",
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "Files",
+        text_align = "left",
+      },
+    },
   }
 }
 
