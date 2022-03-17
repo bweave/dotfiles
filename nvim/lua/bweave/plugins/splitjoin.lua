0f07a0117a7f7f12d43dd76b71d4f5fbe366991c
Brian Weaver <bdrums@gmail.com>
2022-03-17 13:25:31 +0000

    No hanging args with splitjoin

--- nvim/lua/bweave/plugins/splitjoin.lua
+++ nvim/lua/bweave/plugins/splitjoin.lua
@@ -3,6 +3,7 @@ paq {"AndrewRadev/splitjoin.vim"}
 vim.g.splitjoin_split_mapping = ''
 vim.g.splitjoin_join_mapping = ''
 vim.g.splitjoin_trailing_comma = 1
+vim.g.splitjoin_ruby_hanging_args = 0
 
 map('n', '<leader>j', ':SplitjoinSplit<cr>')
 map('n', '<leader>k', ':SplitjoinJoin<cr>')
