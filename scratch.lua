3571be3a9ce2f77d5482a53fe852d3ddeb183e9d
Brian Weaver <bdrums@gmail.com>
2022-04-30 15:26:51 -0400

    Add scratch.lua

--- scratch.lua
+++ scratch.lua
@@ -1,16 +1,54 @@
-local lfs = require"lfs"
-local config_dir = vim.fn.stdpath("config")
+local M = {}
+local Job = require"plenary.job"
+local fzf = require("fzf")
+local helpers = require("fzf.helpers")
 
-function showFolder(folder)
-  print('=====================  ' .. folder)
-  local path = system.pathForFile( folder, system.ResourceDirectory )
-  for file in lfs.dir ( path ) do
-    print (file)
-  end
+local function debug(msg)
+  print(vim.inspect(msg))
 end
 
-showFolder(config_dir)
--- for each file add a mapping that is the first letter of the file
-  -- if more than one being with the same letter, nest
--- for each directory, add a nest
--- loop
+local function handle_stash(action, stash)
+  local job = Job:new({
+    command = "git",
+    args = { "stash", action, stash },
+  })
+  job:sync()
+
+  return job:result()
+end
+
+M.stashes = function()
+  coroutine.wrap(function()
+    local cmd = "git stash list"
+    local preview = helpers.choices_to_shell_cmd_previewer(function(items)
+      local stash = string.gsub(items[1], ":.*", "")
+      return "git show --patience --stat --pretty=oneline --color=always -p " .. vim.fn.shellescape(tostring(stash))
+    end)
+
+    cli_args = table.concat({
+      '--bind="ctrl-f:preview-page-down"',
+      '--bind="ctrl-b:preview-page-up"',
+      '--bind="ctrl-k:preview-up"',
+      '--bind="ctrl-j:preview-down"',
+      '--expect=ctrl-a,ctrl-p,del',
+      '--preview ' .. preview,
+    }, " ")
+
+    local result = fzf.fzf(cmd, cli_args)
+    if result then
+      if result[1] == "ctrl-a" then
+        debug("Gonna apply stash: " .. result[2])
+        handle_stash("apply", result[2])
+      elseif result[1] == "ctrl-p" then
+        debug("Gonna pop stash: " .. result[2])
+        handle_stash("pop", result[2])
+      elseif result[1] == "del" then
+        debug("Gonna drop stash: " .. result[2])
+        handle_stash("drop", result[2])
+      else
+        debug("Gonna handle stash: " .. result[1])
+        handle_stash("apply", result[1])
+      end
+    end
+  end)()
+end
