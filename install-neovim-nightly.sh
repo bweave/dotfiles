f07cd3d35ef3a9d203a233fb5240abe737ccee77
Brian Weaver <bdrums@gmail.com>
2022-09-14 07:52:17 -0400

    wezterm FTW and install neovim from source

--- install-neovim-nightly.sh
+++ install-neovim-nightly.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 cd /tmp || exit
-rm ./nvim-macos.tar.gz
-rm -rf ./nvim-macos/
-gh release download nightly --pattern nvim-macos.tar.gz --repo neovim/neovim
-xattr -c ./nvim-macos.tar.gz # avoid macOS not developer signed warnings
-tar xzvf ./nvim-macos.tar.gz
-cd /usr/local/ || exit
-sudo rm -rf ./nvim-macos
-sudo mv /tmp/nvim-macos /usr/local/nvim-macos
-ln -sf /usr/local/nvim-macos/bin/nvim "$HOMEBREW_PREFIX/bin/nvim"
+# make sure we've got a clean slate
+rm ./neovim-nightly.tar.gz
+rm -rf ./neovim-nightly
+
+# download and untar the source code
+gh release download nightly --archive=tar.gz --repo neovim/neovim
+tar xzvf ./neovim-nightly.tar.gz
+
+# build from source
+cd /tmp/neovim-nightly || exit
+rm -r build/  # clear the CMake cache in case there's anything stale
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+
+# link it so it's in PATH
+cd "$HOME/neovim" || exit
+ln -sf "$HOME/neovim/bin/nvim" "$HOMEBREW_PREFIX/bin/nvim"
