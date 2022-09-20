215b8b882381fb6ef92745465f81dded20624f9a
Brian Weaver <bdrums@gmail.com>
2022-09-20 11:19:32 -0400

    Some wezterm config updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local colors, _ = wezterm.color.load_scheme(wezterm.home_dir .. "/.config/wezterm/base16.toml")
 
 wezterm.on("update", function(window, _)
@@ -13,27 +14,36 @@ return {
 	font_size = 14.0,
 	keys = {
 		-- Splits
-		{ key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical },
-		{ key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal },
+		{ key = "d", mods = "CMD|SHIFT", action = act.SplitVertical },
+		{ key = "d", mods = "CMD", action = act.SplitHorizontal },
+		-- Panes
+		{ key = "s", mods = "LEADER", action = act.PaneSelect },
+		{ key = "S", mods = "LEADER", action = act.PaneSelect({ mode = "SwapWithActive" }) },
 		-- Nav
-		{ key = "]", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Next") },
-		{ key = "[", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Prev") },
+		{ key = "]", mods = "CMD", action = act.ActivatePaneDirection("Next") },
+		{ key = "[", mods = "CMD", action = act.ActivatePaneDirection("Prev") },
+		{ key = "{", mods = "SHIFT|OPT", action = act.MoveTabRelative(-1) },
+		{ key = "}", mods = "SHIFT|OPT", action = act.MoveTabRelative(1) },
 		-- Resizing
-		{ key = "h", mods = "CTRL|SHIFT|OPT", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
-		{ key = "j", mods = "CTRL|SHIFT|OPT", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
-		{ key = "k", mods = "CTRL|SHIFT|OPT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
-		{ key = "l", mods = "CTRL|SHIFT|OPT", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+		{ key = "h", mods = "CTRL|SHIFT|OPT", action = act.AdjustPaneSize({ "Left", 5 }) },
+		{ key = "j", mods = "CTRL|SHIFT|OPT", action = act.AdjustPaneSize({ "Down", 5 }) },
+		{ key = "k", mods = "CTRL|SHIFT|OPT", action = act.AdjustPaneSize({ "Up", 5 }) },
+		{ key = "l", mods = "CTRL|SHIFT|OPT", action = act.AdjustPaneSize({ "Right", 5 }) },
 		-- Clears the scrollback and viewport, and then sends CTRL-L to ask the
 		-- shell to redraw its prompt
 		{
 			key = "k",
 			mods = "CMD",
-			action = wezterm.action.Multiple({
-				wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-				wezterm.action.SendKey({ key = "L", mods = "CTRL" }),
+			action = act.Multiple({
+				act.ClearScrollback("ScrollbackAndViewport"),
+				act.SendKey({ key = "L", mods = "CTRL" }),
 			}),
 		},
+		-- Windows
+		-- { key = "`", mods = "CMD", action = act.SwitchWorkspaceRelative(1) },
+		-- { key = "`", mods = "SHIFT|CMD", action = act.SwitchWorkspaceRelative(-1) },
 	},
+	leader = { key = "a", mods = "CTRL|SHIFT|OPT", timeout_milliseconds = 1000 },
 	line_height = 1.3,
 	window_frame = {
 		font_size = 14.0,
