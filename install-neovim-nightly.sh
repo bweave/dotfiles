f2b45819ffa5526c1e80fbd047199bc229eff21e
Brian Weaver <bdrums@gmail.com>
2022-09-20 11:19:16 -0400

    Get it working for real

--- install-neovim-nightly.sh
+++ install-neovim-nightly.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+filename="neovim-nightly"
+
 cd /tmp || exit
 # make sure we've got a clean slate
-rm -f ./neovim-nightly.tar.gz
-rm -rf ./neovim-nightly
+rm -f ./$filename.tar.gz
+rm -rf ./$filename
 
 # download and untar the source code
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzvf ./nvim-linux64.tar.gz
+curl -fsSL -o $filename.tar.gz https://github.com/neovim/neovim/archive/refs/tags/nightly.tar.gz
+tar xzvf ./$filename.tar.gz
 
 # build from source
-cd /tmp/nvim-linux64 || exit
+cd /tmp/$filename || exit
 rm -rf build/  # clear the CMake cache in case there's anything stale
 make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 make install
 
 # link it so it's in PATH
+mkdir -p "$HOME/neovim"
 cd "$HOME/neovim" || exit
 ln -sf "$HOME/neovim/bin/nvim" "${HOMEBREW_PREFIX:-/usr/local}/bin/nvim"
