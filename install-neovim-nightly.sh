016a3a562821e5184f00210cb7fa0a26c70e76d3
Brian Weaver <bdrums@gmail.com>
2022-09-26 11:57:05 -0400

    Install nvim from the binary tar.gz vs. building from source

--- install-neovim-nightly.sh
+++ install-neovim-nightly.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-filename="neovim-nightly"
+platform=$(case "$OSTYPE" in
+  (linux*) echo "linux64";;
+  (darwin*) echo "macos";;
+  (*) exit 1;;
+esac)
 
 cd /tmp || exit
-# make sure we've got a clean slate
-rm -f ./$filename.tar.gz
-rm -rf ./$filename
-
-# download and untar the source code
-curl -fsSL -o $filename.tar.gz https://github.com/neovim/neovim/archive/refs/tags/nightly.tar.gz
-tar xzvf ./$filename.tar.gz
-
-# build from source
-cd /tmp/$filename || exit
-rm -rf build/  # clear the CMake cache in case there's anything stale
-make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
-make install
-
-# link it so it's in PATH
-mkdir -p "$HOME/neovim"
-cd "$HOME/neovim" || exit
-ln -sf "$HOME/neovim/bin/nvim" "${HOMEBREW_PREFIX:-/usr/local}/bin/nvim"
+rm -rf "./nvim-$platform.tar.gz"
+rm -rf "./nvim-$platform/"
+curl -fsSL -O "https://github.com/neovim/neovim/releases/download/nightly/nvim-$platform.tar.gz"
+if [ "$platform" == "macos" ]; then
+  xattr -c "./nvim-$platform.tar.gz" # avoid macOS not developer signed warnings
+fi
+tar xzvf "./nvim-$platform.tar.gz"
+cd /usr/local/ || exit
+sudo rm -rf "./nvim-$platform"
+sudo mv "/tmp/nvim-$platform" "/usr/local/nvim-$platform"
+ln -sf "/usr/local/nvim-$platform/bin/nvim" /usr/local/bin/nvim
+echo "Done!"
