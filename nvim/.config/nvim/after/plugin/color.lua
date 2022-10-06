648a20bfc3434dbc88c20a92f72f68c342f16610
Brian Weaver <bdrums@gmail.com>
2022-10-06 16:12:38 -0400

    Refinements to setting colorschemes

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -1,8 +1,6 @@
 --------------------------------------------------------------------------
--- Colors
+-- color
 --------------------------------------------------------------------------
-vim.cmd("colorscheme base16-decaf")
-
 local M = {}
 
 local status, fzf = pcall(require, "fzf")
@@ -16,48 +14,71 @@ local function debug(msg)
 end
 
 -- this is our single source of truth created above
-local base16_theme_fname = vim.fn.expand(vim.env.HOME .. "/.base16_theme")
+local base16_filename = vim.fn.expand(vim.env.HOME .. "/.base16_theme")
 
--- this function is the only way we should be setting our colorscheme
-M.set_colorscheme = function(name)
-	local HOME = vim.env.HOME
-	local go_path = vim.fn.system({ "go", "env", "GOPATH" }):gsub("\n", "")
-	local cmd = string.format("%s/bin/base16-universal-manager", go_path)
-	local config = string.format("--config=%s/.config/base16-universal-manager/config.yaml", HOME)
-	local scheme = string.format("--scheme=%s", name:match("-([%w-]+)"))
+local get_colorscheme = function()
+	local file = io.open(base16_filename, "r")
+	local theme = "default"
 
-	-- set global base16 theme
-	vim.fn.system({ cmd, config, scheme })
+	if file ~= nil then
+		theme = file:read()
+		file:close()
+	end
 
-	-- tell all nvims to reload updated base16 theme
+	return theme
+end
+vim.cmd("colorscheme " .. get_colorscheme())
+
+local set_global_base16_theme = function(theme)
+	local file = io.open(base16_filename, "w")
+
+	if file ~= nil then
+		file:write(theme)
+		file:close()
+	end
+end
+
+local set_all_nvim_colorschemes = function(colorscheme)
 	for socket, kind in vim.fs.dir("/tmp") do
 		if kind == "socket" and socket:find("nvimsocket") then
 			local server = string.format("/tmp/%s", socket)
-			local remote_send = ":colo base16<cr>"
-
+			local remote_send = string.format(":colorscheme %s<cr>", colorscheme)
 			vim.fn.system({ "nvim", "--server", server, "--remote-send", remote_send })
 		end
 	end
+end
 
-	-- TODO: tell tmux to reload its base16 theme
-	-- tell tmux to reload its base16 theme
-	-- if vim.env.TMUX then
-	-- 	vim.fn.system({
-	-- 		"tmux",
-	-- 		"source-file",
-	-- 		string.format("%s/.tmux/plugins/base16-tmux/colors/%s.conf", HOME, name),
-	-- 	})
-	-- end
+local set_all_iterm_color_presets = function(preset)
+	-- TODO
+end
+
+local set_tmux_theme = function(theme)
+	if vim.env.TMUX then
+		vim.fn.system({
+			"tmux",
+			"source-file",
+			string.format("%s/.tmux/plugins/base16-tmux/colors/%s.conf", vim.env.HOME, theme),
+		})
+	end
+end
+
+-- this function is the only way we should be setting our colorscheme
+M.set_colorscheme = function(name)
+	set_global_base16_theme(name)
+	set_all_nvim_colorschemes(name)
+	set_all_iterm_color_presets(name)
+	set_tmux_theme(name)
 end
 
 M.select_colorscheme = function()
 	coroutine.wrap(function()
+		local current_background = vim.o.background
 		local current_colorscheme = vim.g.colors_name or "default"
 		local colors = vim.list_extend({}, vim.fn.getcompletion("base16", "color"))
 
 		local preview = helpers.choices_to_shell_cmd_previewer(function(items)
-			vim.cmd("colo " .. items[1])
-		end, nil, false)
+			vim.cmd("colorscheme " .. items[1])
+		end, nil)
 		local cli_args = table.concat({
 			"--no-multi",
 			"--preview-window nohidden:right:0",
@@ -69,7 +90,8 @@ M.select_colorscheme = function()
 		if result then
 			M.set_colorscheme(result[1])
 		else
-			vim.cmd("colo " .. current_colorscheme)
+			vim.o.background = current_background
+			vim.cmd("colorscheme " .. current_colorscheme)
 		end
 	end)()
 end
