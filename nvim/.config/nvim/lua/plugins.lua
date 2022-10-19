89f87457998c862cb254dfe3af3279bbc3c46811
Brian Weaver <bdrums@gmail.com>
2022-10-19 09:07:39 -0400

    Fix fzf config and pull it out into its own file
    Otherwise, we can only update its config when updating Packer, and
    that's dumb.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -89,25 +89,6 @@ packer.startup({
 		use({
 			"ibhagwan/fzf-lua",
 			requires = { "kyazdani42/nvim-web-devicons" },
-			config = function()
-				local status, fzf_lua = pcall(require, "fzf-lua")
-				if not status then
-					return
-				end
-
-				fzf_lua.setup({
-					winopts = {
-						height = 0.40, -- window height
-						width = 1, -- window width
-						row = 1, -- window row position (0=top, 1=bottom)
-						col = 0, -- window col position (0=left, 1=right)
-					},
-					fzf_opts = {
-						["--info"] = "hidden",
-						["--layout"] = false,
-					},
-				})
-			end,
 		})
 		use({ "vijaymarupudi/nvim-fzf" })
 		use({
