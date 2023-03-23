d08050eb247ab41ee1dd7d7361e41085a6696d85
Brian Weaver <bdrums@gmail.com>
2023-03-23 05:51:22 -0400

    Rust setup

---
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -0,0 +1,10 @@
+--
+-- after/ftplugin/rust.lua
+--
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+	group = vim.api.nvim_create_augroup("BwRustAutocmds", { clear = true }),
+})
