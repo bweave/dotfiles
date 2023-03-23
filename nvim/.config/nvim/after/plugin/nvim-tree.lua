4458441ecd4aa2cfbfa4090450b15ec85483eb9e
Brian Weaver <bdrums@gmail.com>
2023-03-23 05:50:30 -0400

    nvim-tree lint/formatting

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -5,20 +5,20 @@
 local nvim_tree_ok, nvim_tree = pcall(require, "nvim-tree")
 
 if not nvim_tree_ok then
-  print("!! nvim-tree couldn't be required !!")
-  return
+	print("!! nvim-tree couldn't be required !!")
+	return
 end
 
 nvim_tree.setup({
-  renderer = {
-    root_folder_modifier = ":t",
-  },
-  view = {
-    hide_root_folder = false,
-    mappings = {
-      list = {
-        { key = "?", action = "toggle_help" },
-      },
-    },
-  },
+	renderer = {
+		root_folder_modifier = ":t",
+	},
+	view = {
+		hide_root_folder = false,
+		mappings = {
+			list = {
+				{ key = "?", action = "toggle_help" },
+			},
+		},
+	},
 })
