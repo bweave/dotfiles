281a761ea6f98414b61634b1ee3db4a6acff0873
Brian Weaver <bdrums@gmail.com>
2023-10-14 14:25:21 -0400

    Open Asana & Notion links in desktop app

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,7 +4,7 @@ local meh = { "⌥", "⌃", "⇧" }
 
 -- Always find the right app id
 local function appID(app)
-	return hs.application.infoForBundlePath(app)["CFBundleIdentifier"]
+  return hs.application.infoForBundlePath(app)["CFBundleIdentifier"]
 end
 
 -- Spoons
@@ -16,19 +16,25 @@ Install = spoon.SpoonInstall
 Install:andUse("ReloadConfiguration", { start = true })
 
 -- Open URLs in specific apps
+local asana = appID("/Applications/Asana.app")
 local browser = appID("/Applications/Google Chrome.app")
+local notion = appID("/Applications/Notion.app")
 local spotify = appID("/Applications/Spotify.app")
 
 Install:andUse("URLDispatcher", {
-	config = {
-		default_handler = browser,
-		url_patterns = {
-			{ "spotify:", spotify },
-		},
-		url_redir_decoders = {
-			{ "Spotify URLs", "https://open.spotify.com/(.*)/(.*)", "spotify:%1:%2" },
-		},
-	},
-	start = true,
-	-- loglevel = "debug",
+  config = {
+    default_handler = browser,
+    url_patterns = {
+      { "asanadesktop:", asana },
+      { "notion:", notion },
+      { "spotify:", spotify },
+    },
+    url_redir_decoders = {
+      { "Asana URLs", "https://app.asana.com/(.*)", "asanadesktop:///app/%1" },
+      { "Notion URLs", "https://(www.notion.so/.*)", "notion://%1" },
+      { "Spotify URLs", "https://open.spotify.com/(.*)/(.*)", "spotify:%1:%2" },
+    },
+  },
+  start = true,
+  loglevel = "debug",
 })
